7d13bca1a28da0a00a3c8e50062ba0eb6481bff5
Vitor Roma <vitor_roma6@hotmail.com>
2024-06-05 11:26:34 -0300

    feat: adding Login feature.

---
+++ src/main/java/vr/app/bussinesmanager/LoginController.java
@@ -0,0 +1,11 @@
+package vr.app.bussinesmanager;
+
+import vr.app.model.Model;
+
+public class LoginController {
+    private Model model;
+
+    public LoginController(Model model) {
+        this.model = model;
+    }
+}

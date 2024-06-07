97621cd3cd9b84809b82b65c1d4bb3282718008c
Vitor Roma <vitor_roma6@hotmail.com>
2024-06-07 11:19:52 -0300

    style: added view constraints to Constants class

---
+++ src/main/java/vr/app/common/Constants.java
@@ -0,0 +1,16 @@
+package vr.app.common;
+
+public class Constants {
+
+    public Constants() {
+    }
+
+    public static final double marginTopAndBottom = 5.00;
+    public static final double marginLeftAndRight = 15.00;
+
+    public static final double textFieldWidth = 200.00;
+    public static final double textFieldHeight = 50.00;
+
+    public static final double buttonWidth = 75.00;
+    public static final double buttonHeight = 75.00;
+}

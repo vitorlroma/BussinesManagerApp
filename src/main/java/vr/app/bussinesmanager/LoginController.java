2ce1e6ab297e16093cc57001bf29d23b092707e6
Vitor Roma <vitor_roma6@hotmail.com>
2024-06-07 11:21:07 -0300

    refactor: WIP, adding function to controller

--- src/main/java/vr/app/bussinesmanager/LoginController.java
+++ src/main/java/vr/app/bussinesmanager/LoginController.java
@@ -2,10 +2,24 @@ package vr.app.bussinesmanager;
 
 import vr.app.model.Model;
 
-public class LoginController {
+public final class LoginController {
     private Model model;
+    private String username;
+    private String password;
 
     public LoginController(Model model) {
         this.model = model;
+    }
+
+    public void Login(){
+
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }

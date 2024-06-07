6cea73c9c496aadc8f4edc4e5dcf7d3b29a1e966
Vitor Roma <vitor_roma6@hotmail.com>
2024-06-07 11:19:13 -0300

    refactor: added constraints to a constants file, finished view code, needs visual changes only.

--- src/main/java/vr/app/view/LoginScreen.java
+++ src/main/java/vr/app/view/LoginScreen.java
@@ -1,5 +1,7 @@
 package vr.app.view;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -8,47 +10,57 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import vr.app.bussinesmanager.LoginController;
 import vr.app.model.Model;
+import static vr.app.common.Constants.*;
 
-public class LoginScreen {
-    private LoginController loginController;
+public final class LoginScreen {
+    private final LoginController loginController;
     private Model model;
     private VBox view;
-    private VBox userVBox;
-    private VBox passwordVBox;
     private final Label userLabel = new Label("Username: ");
     private TextField usernameField;
     private final Label passwordLabel = new Label("Password: ");
     private PasswordField passwordField;
     private Button loginButton;
 
-    public LoginScreen(LoginController loginController, Model model) {
+    public LoginScreen(LoginController loginController) {
         this.loginController = loginController;
-        this.model = model;
 
         build();
+
+        updateController();
     }
 
     public Parent asParent() {
         return view;
     }
+
     private void build() {
         this.usernameField = new TextField();
         this.usernameField.setPromptText("username");
-        this.usernameField.setMaxSize(100.00,50.00);
-        this.usernameField.setPrefSize(100.00, 50.00);
+        this.usernameField.setMaxSize(textFieldWidth*2, textFieldHeight*2);
+        this.usernameField.setPrefSize(textFieldWidth, textFieldHeight);
 
         this.passwordField = new PasswordField();
         this.passwordField.setPromptText("password");
-        this.passwordField.setMaxSize(100.00,50.00);
-        this.passwordField.setPrefSize(100.00, 50.00);
+        this.passwordField.setMaxSize(textFieldWidth*2,textFieldHeight*2);
+        this.passwordField.setPrefSize(textFieldWidth, textFieldHeight);
 
-        this.userVBox = new VBox(userLabel, usernameField);
-        this.passwordVBox = new VBox(passwordLabel, passwordField);
+        VBox userVBox = new VBox( 5.0, userLabel, usernameField);
+        VBox passwordVBox = new VBox(5.0, passwordLabel, passwordField);
 
         this.loginButton = new Button("Login");
-        this.loginButton.setMaxSize(50.00, 50.00);
-        this.loginButton.setDefaultButton(true);
+        loginButton.setMaxSize(buttonWidth, buttonHeight);
+        loginButton.setDefaultButton(true);
+        loginButton.setOnAction(event -> loginController.Login());
 
-        this.view = new VBox(15.00, userVBox,passwordVBox,loginButton);
+        this.view = new VBox(15.00, userVBox, passwordVBox, loginButton);
+        this.view.setAlignment(Pos.CENTER);
+        VBox.setMargin(userVBox, new Insets(marginTopAndBottom, marginLeftAndRight, marginTopAndBottom, marginLeftAndRight));
+        VBox.setMargin(passwordVBox, new Insets(marginTopAndBottom, marginLeftAndRight, marginTopAndBottom, marginLeftAndRight));
+    }
+
+    private void updateController(){
+        this.usernameField.textProperty().addListener((observable, oldValue, newValue) -> loginController.setUsername(newValue));
+        this.passwordField.textProperty().addListener((observable, oldValue, newValue) -> loginController.setPassword(newValue));
     }
 }
